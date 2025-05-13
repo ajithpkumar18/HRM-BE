7ee67310cb92c647c2d7c0d15967be1569d67ca4
AJITH P. KUMAR <77005115+ajithpkumar18@users.noreply.github.com>
2025-05-13 15:38:10 +0530

    Delete dist directory
    dist folder not required in github, dist folder can be generated using the command "npm run dev"

--- dist/routes/route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../middlewares/auth");
-const router = (0, express_1.Router)();
-router.get("/dashboard", auth_1.authMiddleware, (req, res, next) => {
-    res.send("Hello World!");
-});
--- dist/utils/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatTime = formatTime;
-function formatTime(date) {
-    return date.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
-}
